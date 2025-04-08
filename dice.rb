4f97f5570fcf890a6cd9b99fcaf2914d4843d1e6
elliewilson1 <ewilson0@chicagobooth.edu>
2025-04-08 23:11:18 +0000

    homepage formatting

--- dice.rb
+++ dice.rb
@@ -3,8 +3,14 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do 
-  "Hello world"
+get("/") do
+  return '<h1>Dice Roll</h1>
+          <ul>
+            <li><a href = "/dice/2/6" target="_blank">Roll two 6-sided dice</a></li>
+            <li><a href = "/dice/2/10" target="_blank">Roll two 10-sided dice</a></li>
+            <li><a href = "/dice/1/20" target="_blank">Roll one 20-sided die</a></li>
+            <li><a href = "/dice/5/4" target="_blank">Roll five 4-sided dice</a></li>
+          </ul>'
 end
 
 get("/zebra") do
