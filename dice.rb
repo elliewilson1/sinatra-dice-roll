539e8d838fb959f50101d88d867de7f42c6660e7
elliewilson1 <ewilson0@chicagobooth.edu>
2025-04-08 23:36:55 +0000

    finished

--- dice.rb
+++ dice.rb
@@ -2,24 +2,33 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  return '<h1>Dice Roll</h1>
+  return '<style> div { display: flex; justify-content: space-around} </style>
+          <div><a href = "/">Home</a><a href = "/dice/2/6">2d6</a><a href = "/dice/2/10">2d10</a><a href = "/dice/1/20">1d20</a><a href = "/dice/5/4">5d4</a></div>
+          <h1>Dice Roll</h1>
           <ul>
-            <li><a href = "/dice/2/6" target="_blank">Roll two 6-sided dice</a></li>
-            <li><a href = "/dice/2/10" target="_blank">Roll two 10-sided dice</a></li>
-            <li><a href = "/dice/1/20" target="_blank">Roll one 20-sided die</a></li>
-            <li><a href = "/dice/5/4" target="_blank">Roll five 4-sided dice</a></li>
+            <li><a href = "/dice/2/6">Roll two 6-sided dice</a></li>
+            <li><a href = "/dice/2/10">Roll two 10-sided dice</a></li>
+            <li><a href = "/dice/1/20">Roll one 20-sided die</a></li>
+            <li><a href = "/dice/5/4">Roll five 4-sided dice</a></li>
           </ul>'
 end
 
-get("/zebra") do
-  "We must add a route for each path we want to support"
-end
+# get("/zebra") do
+#  "We must add a route for each path we want to support"
+# end
 
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server"
-end
+# get("/giraffe") do
+#  "Hopefully this shows up without having to restart the server"
+# end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
@@ -28,7 +37,15 @@ get("/dice/2/6") do
 
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
+  "<style> div { display: flex; justify-content: space-around } </style>
+  <div>
+    <a href='/'>Home</a>
+    <a href='/dice/2/6'>2d6</a>
+    <a href='/dice/2/10'>2d10</a>
+    <a href='/dice/1/20'>1d20</a>
+    <a href='/dice/5/4'>5d4</a>
+  </div>
+  <h1>2d6</h1>
   <p>#{outcome}</p>"
 end 
 
@@ -39,7 +56,15 @@ get("/dice/2/10") do
 
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d10</h1>
+  "<style> div { display: flex; justify-content: space-around } </style>
+  <div>
+    <a href='/'>Home</a>
+    <a href='/dice/2/6'>2d6</a>
+    <a href='/dice/2/10'>2d10</a>
+    <a href='/dice/1/20'>1d20</a>
+    <a href='/dice/5/4'>5d4</a>
+  </div>
+  <h1>2d10</h1>
   <p>#{outcome}</p>"
 end
 
@@ -48,7 +73,15 @@ get("/dice/1/20") do
 
   outcome = "You rolled a #{first_die}."
 
-  "<h1>1d20</h1>
+  "<style> div { display: flex; justify-content: space-around } </style>
+  <div>
+    <a href='/'>Home</a>
+    <a href='/dice/2/6'>2d6</a>
+    <a href='/dice/2/10'>2d10</a>
+    <a href='/dice/1/20'>1d20</a>
+    <a href='/dice/5/4'>5d4</a>
+  </div>
+  <h1>1d20</h1>
   <p>#{outcome}</p>"
 end
 
@@ -62,6 +95,14 @@ get("/dice/5/4") do
 
   outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
 
-  "<h1>5d4</h1>
+  "<style> div { display: flex; justify-content: space-around } </style>
+  <div>
+    <a href='/'>Home</a>
+    <a href='/dice/2/6'>2d6</a>
+    <a href='/dice/2/10'>2d10</a>
+    <a href='/dice/1/20'>1d20</a>
+    <a href='/dice/5/4'>5d4</a>
+  </div>
+  <h1>5d4</h1>
   <p>#{outcome}</p>"
 end 
